f1799de77f8f81e746799990fa614875923f3788
malgo-slota <malgo.slota@gmail.com>
2022-05-09 13:41:36 +0200

    refactor

--- index.js
+++ index.js
@@ -1,90 +1,56 @@
-
 const baseApiUrl = `http://www.omdbapi.com/`;
-
-const fetchData = async searchPhrase => {
-	try {	
-		const response = await axios.get(baseApiUrl, {
-            params: {
-                apikey: config.SECRET_API_KEY,
-                s: searchPhrase
-            }
-        });
-
-        if(response.data.Error) {
-            return [];
-        }
-		return response.data.Search;
-
-} catch(err) {
-	console.log("ERROR", err)
-    }
-};
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />   
-    
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results">
-            </div>
-        </div>
-      </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value); 
-    
-    if(!movies.length){
-        dropdown.classList.remove('is-active');
-        return;
-    }
-    
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for (let movie of movies) {
-        const option = document.createElement('a');
+const autoCompleteConfig = {
+    renderOption(movie) {
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
+        return `
             <img src="${imgSrc}" />
-            ${movie.Title}
+            ${movie.Title} (${movie.Year})
         `;
+    },
+    inputValue(movie) {
+        return movie.Title;
+    },
+    async fetchData (searchPhrase) {  	
+            const response = await axios.get(baseApiUrl, {
+                params: {
+                    apikey: config.SECRET_API_KEY,
+                    s: searchPhrase
+                }
+            });
 
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-
-        resultsWrapper.appendChild(option);
+            if(response.data.Error) {
+                return [];
+            }
+            return response.data.Search; 
     }
 };
 
-input.addEventListener('input', debounce(onInput));
-
-document.addEventListener('click', event => {
-    if(!root.contains(event.target)){
-        dropdown.classList.remove('is-active');
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+     onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#left-summary'));
     }
 });
 
-const onMovieSelect = async movie => {
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+     onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie, document.querySelector('#right-summary'));
+    }
+});
+
+const onMovieSelect = async (movie, summaryElement) => {
     const response = await axios.get(baseApiUrl, {
         params: {
             apikey: config.SECRET_API_KEY,
             i: movie.imdbID
         }
     });
-
-    // console.log(response.data);
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+    summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
